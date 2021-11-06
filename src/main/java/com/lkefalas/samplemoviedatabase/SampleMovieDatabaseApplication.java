f79e820d7863ec40e84e0a9603d92ffbc83efb1b
lamproskefalas <kefalas.lampros@gmail.com>
2021-11-06 18:47:38 +0200

    Initial project structure

---
+++ src/main/java/com/lkefalas/samplemoviedatabase/SampleMovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.lkefalas.samplemoviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMovieDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lkefalas/samplemoviedatabase/SampleMovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkefalas.samplemoviedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
